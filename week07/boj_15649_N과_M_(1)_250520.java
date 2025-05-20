e1caa7c17f923a8127fc0fe73804d773a115223e
SooyeonOh <sooyeon7027@naver.com>
2025-05-20 13:10:03 +0900

    add: week07

---
+++ week07/boj_15649_N과_M_(1)_250520.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N, M;
+	static List<Integer> sel = new ArrayList<>();
+	static StringBuilder sb = new StringBuilder(); 
+	static boolean[] visited;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		input();
+		
+		func(0);
+		
+		bw.write(sb.toString());
+		bw.close();
+	}
+
+	private static void func(int cnt) {
+		
+		if(cnt == M) {
+			printNums();
+		}
+		
+		for(int i=1; i<=N; ++i) {
+			if(visited[i]) continue;
+			sel.add(i);
+			visited[i] = true;
+			func(cnt+1);
+			sel.remove(sel.size()-1);
+			visited[i] = false;
+		}
+		
+	}
+
+	private static void printNums() {
+		for(int i=0; i<sel.size(); ++i) {
+			sb.append(sel.get(i)).append(' ');
+		}
+		sb.append('\n');
+	}
+
+	private static void input() throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		visited = new boolean[N+1];
+	}
+
+}
---
+++ week07/boj_15652_N과_M_(4)_250520.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N, M;
+	static List<Integer> sel = new ArrayList<>();
+	static StringBuilder sb = new StringBuilder(); 
+	
+	public static void main(String[] args) throws Exception{
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		input();
+		
+		func(1, 0);
+		
+		bw.write(sb.toString());
+		bw.close();
+	}
+
+	private static void func(int idx, int cnt) {
+		
+		if(cnt == M) {
+			printNums();
+			return;
+		}
+		
+		for(int i=idx; i<=N; ++i) {
+			sel.add(i);
+			func(i, cnt+1);
+			sel.remove(sel.size()-1);
+		}
+		
+	}
+
+	private static void printNums() {
+		for(int i=0; i<sel.size(); ++i) {
+			sb.append(sel.get(i)).append(' ');
+		}
+		sb.append('\n');
+	}
+
+	private static void input() throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+	}
+
+}
